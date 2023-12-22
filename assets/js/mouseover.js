4f128811194af5fa8d24f49acce6fcc171055c3a
Ioannis Doganos <i.doganos@tutamail.com>
2023-12-22 08:15:14 +0000

    Initial Commit

---
+++ assets/js/mouseover.js
@@ -0,0 +1,4 @@
+// script.js
+function changeLocation(link) {
+    window.location.href = link;
+  }
---
+++ assets/js/timeline2.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+  licenseKey: null, // Set to null to avoid the license key requirement
+  autoScrolling: true, // Enable or disable auto-scrolling
+  fitToSection: false, // Set to false to disable full-page fitting
+  navigation: true,
+  sectionsColor: ['#ff5f45', '#0798ec', '#fc6c7c', '#fec401'],
+});
